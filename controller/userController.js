9eeee90c024aa50154dca85e5a4647917a396a4a
Daljeet singh <daljeetsingh9911@gmail.com>
2023-06-07 12:10:53 +0200

    done with updated user auth time

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,11 @@
 import Users from '../models/users.js'
+
 export const insertUser = async (user,res) =>{
     //wait to get the user information for mongodb
    const isUserExist = await Users.findOne({"user_id":user.user_id});
 
     if(isUserExist !== null){
-        res.status(400).json({status:400,message:"User is already exist update data"});
+         updateUSer(user,res);
     }else{
          //inserting user information to mongodb
         const UserObj = new Users({
@@ -25,3 +26,21 @@ export const insertUser = async (user,res) =>{
     }
 
 }
+
+const updateUSer = async (user,res) =>{
+    const filter = {'user_id':user.user_id};      
+    const options = {
+        upset:true,
+        new:true
+    }
+    await Users.findOneAndUpdate(
+        filter,
+        {auth_time:user.auth_time},
+        options
+    ).then((data)=>{
+        res.status(200).json({status:200,message:"User is successfully  updated",data});
+    }).catch((e)=>{
+        res.status(400).json({status:400,message:e});
+    })
+ }
+
